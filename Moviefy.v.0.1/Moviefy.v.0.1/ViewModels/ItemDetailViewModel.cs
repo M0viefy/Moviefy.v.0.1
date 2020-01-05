1ac7aecdfea635f810b5e9ea003d45a081c9b592
samilalgul <samilalgul@gmail.com>
2020-01-05 21:21:01 +0300

    Add project files.

---
+++ Moviefy.v.0.1/Moviefy.v.0.1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Moviefy.v._0._1.Models;
+
+namespace Moviefy.v._0._1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
